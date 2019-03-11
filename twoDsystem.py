026e9a274a84101ab29ae09ed06e89c06ca80aeb
Michael Johnson <44432163+MichaelDavidJohnson@users.noreply.github.com>
2019-03-11 18:40:47 +0000

    Separatracies added

--- twoDsystem.py
+++ twoDsystem.py
@@ -19,7 +19,9 @@ class twoDSystem:
         self.steadyStates = False
         self.trajectories = False
         self.title = "Title"
-		self.numericalSteadyStates = False
+        self.numericalSteadyStates = False
+        self.Seperatricies = False
+        self.steadyStateArray = []
     
     def setArrows(ArrowNo = 10):
         self.noArrowX = ArrowNo
@@ -58,12 +60,11 @@ class twoDSystem:
 
 
     def setSteadyStates(self,steadyStateX,steadyStateY,colour = "r"):
-        self.steadyStateX = (steadyStateX)
-        self.steadyStateY = (steadyStateY)
+        self.steadyStateArray = list(zip(steadyStateX,steadyStateY))
         if colour == "r" or colour == "b" or colour == "g" or colour == "c" or colour == "m" or colour == "y" or colour == "b" or colour == "w":
-			self.steadyStateColour = colour
-		else:
-			print("Invalid colour choice, see .plotoptions() for help.")
+            self.steadyStateColour = colour
+        else:
+            print("Invalid colour choice, see .plotoptions() for help.")
         self.steadyStates = True
         
     def setDiffs(self,fx,fy,gx,gy):
@@ -93,15 +94,15 @@ class twoDSystem:
             dy2dt = self.g(y1,y2)
             return [dy1dt,dy2dt]
         sol = self.solve_ivp(dydt,[minT,maxT],[initialX,initialY])
-		
+        
         return sol
     
-	def setFlowColour(self,colour = "g"):
+    def setFlowColour(self,colour = "g"):
         if colour == "r" or colour == "b" or colour == "g" or colour == "c" or colour == "m" or colour == "y" or colour == "b" or colour == "w":
-			self.flowColour = colour
-		else:
-			print("Invalid colour choice, see .plotoptions() for help.")
-	
+            self.flowColour = colour
+        else:
+            print("Invalid colour choice, see .plotoptions() for help.")
+
     def newtonRaphson(self,testX,testY,accuracy = 0.00001):
         self.testX = testX
         self.testY = testY
@@ -123,7 +124,7 @@ class twoDSystem:
             self.testY = self.testY - tempArray[1][0]
             eps = np.sqrt( (self.f(x = self.testX, y = self.testY))**2 + (self.g(x = self.testX, y = self.testY))**2 )
         print("There is a steady state at ", [self.testX,self.testY])
-		
+
         return self.testX,self.testY
     
     def newtonRaphsonAnalytic(self,testX,testY,accuracy = 0.00001):
@@ -138,7 +139,7 @@ class twoDSystem:
             tY -=  tempArray[1][0]
             eps = np.sqrt( (self.f(x = tX, y = tY))**2 + (self.g(x = tX, y = tY))**2 )
         print("There is a steady state at ", [tX,tY])
-		
+
         return tX,tY
 
     def findSteadyStates(self,epsilon = 0.01,method = "Exhaustive"):
@@ -161,94 +162,134 @@ class twoDSystem:
 
             print(self.steadyStateArray)
             self.numericalSteadyStates = True
-			
+            
         if method == "Analytic":
-            self.steadyStateXArray = []
-			def frange(x,y,jump):
-				while x<y:
-					yield x
-					x += jump
-			for y in frange(self.yMin,self.yMax,epsilon):
-				for x in frange(self.xMin,self.xMax,epsilon):
-					tempX,tempY = self.newtonRaphsonAnalytic(x,y)
-					isInCell = (tempX > x - 0.5*epsilon) and (tempX <= x + 0.5*epsilon) and (tempY > y - 0.5*epsilon) and (tempY <= y + 0.5*epsilon)
-					if isInCell:
+            
+            def frange(x,y,jump):
+                while x<y:
+                    yield x
+                    x += jump
+            for y in frange(self.yMin,self.yMax,epsilon):
+                for x in frange(self.xMin,self.xMax,epsilon):
+                    tempX,tempY = self.newtonRaphsonAnalytic(x,y)
+                    isInCell = (tempX > x - 0.5*epsilon) and (tempX <= x + 0.5*epsilon) and (tempY > y - 0.5*epsilon) and (tempY <= y + 0.5*epsilon)
+                    if isInCell:
                         self.steadyStateArray.append([tempX,tempY])
        
     
     def getStability(self):
         
         if self.numericalSteadyStates == False:
-			def partial_derivative(func, var=0, point=[]):
-				args = point[:]
-				def wraps(x):
-					args[var] = x
-					return func(*args)
-				return derivative(wraps, point[var], dx = 1e-14)
+            def partial_derivative(func, var=0, point=[]):
+                args = point[:]
+                def wraps(x):
+                    args[var] = x
+                    return func(*args)
+                return derivative(wraps, point[var], dx = 1e-14)
         
-			for n in range(len(self.steadyStateX)):
-				Jacobfx = partial_derivative(f,0,[self.steadyStateX[n],self.steadyStateY[n]])
-				Jacobgx = partial_derivative(g,0,[self.steadyStateX[n],self.steadyStateY[n]])
-				Jacobfy = partial_derivative(f,1,[self.steadyStateX[n],self.steadyStateY[n]])
-				Jacobgy = partial_derivative(g,1,[self.steadyStateX[n],self.steadyStateY[n]])
-				trace = Jacobfx + Jacobgy
-				det = ( Jacobfx * Jacobgy ) - ( Jacobfy * Jacobgx )
-				disc = trace**2 - 4*det
-				if det < 0:
-					print("The Steady State at ",(self.steadyStateX[n],self.steadyStateY[n]),"is a Saddle Node")
-				elif det > 0 and trace == 0:
-					print("The Steady State at ",(self.steadyStateX[n],self.steadyStateY[n])," is a Centre")
-				elif det > 0 and trace > 0 and disc > 0:
-					print("The Steady State at ",(self.steadyStateX[n],self.steadyStateY[n])," is an Unstable Node")
-				elif det > 0 and trace < 0 and disc > 0:
-					print("The Steady State at ",(self.steadyStateX[n],self.steadyStateY[n])," is a Stable Node")
-				elif det > 0 and trace > 0 and disc < 0:
-					print("The Steady State at ",(self.steadyStateX[n],self.steadyStateY[n]), " is an Unstable Focus")
-				elif det > 0 and trace < 0 and disc < 0:
-					print("The Steady State at ",(self.steadyStateX[n],self.steadyStateY[n])," is a Stable Focus")
+            for steadyState in self.steadyStateArray:
+                Jacobfx = partial_derivative(f,0,steadyState)
+                Jacobgx = partial_derivative(g,0,steadyState)
+                Jacobfy = partial_derivative(f,1,steadyState)
+                Jacobgy = partial_derivative(g,1,steadyState)
+                trace = Jacobfx + Jacobgy
+                det = ( Jacobfx * Jacobgy ) - ( Jacobfy * Jacobgx )
+                disc = trace**2 - 4*det
+                if det < 0:
+                    print("The Steady State at ",steadyState,"is a Saddle Node")
+                elif det > 0 and trace == 0:
+                    print("The Steady State at ",steadyState," is a Centre")
+                elif det > 0 and trace > 0 and disc > 0:
+                    print("The Steady State at ",steadyState," is an Unstable Node")
+                elif det > 0 and trace < 0 and disc > 0:
+                    print("The Steady State at ",steadyState," is a Stable Node")
+                elif det > 0 and trace > 0 and disc < 0:
+                    print("The Steady State at ",steadyState, " is an Unstable Focus")
+                elif det > 0 and trace < 0 and disc < 0:
+                    print("The Steady State at ",steadyState," is a Stable Focus")
         elif self.numericalSteadyStates == True:
-			def partial_derivative(func,var = 0,point = []):
-				args = point[:]
-				def wraps(x):
-					args[var] = x
-					return funct(*args)
-				return derivative(wraps,point[var],dx = 1e-14
-			for n in range(len(self.steadyStateArray)):
-				Jacobfx = partial_derivative(f,0,[self.steadyStateArray[n,0],self.steadyStateArray[n,1]])
-				Jacobgx = partial_derivative(g,0,[self.steadyStateArray[n,0],self.steadyStateArray[n,1]])
-				Jacobfy = partial_derivative(f,1,[self.steadyStateArray[n,0],self.steadyStateArray[n,1]])
-				Jacobgy = partial_derivative(g,1,[self.steadyStateArray[n,0],self.steadyStateArray[n,1]])
-				trace = Jacobfx + Jacobgy
-				det = ( Jacobfx * Jacobgy ) - ( Jacobfy * Jacobgx )
-				disc = trace**2 - 4*det			
-				if det < 0:
-					print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1]),"is a Saddle Node")
-				elif det > 0 and trace == 0:
-					print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1])," is a Centre")
-				elif det > 0 and trace > 0 and disc > 0:
-					print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1])," is an Unstable Node")
-				elif det > 0 and trace < 0 and disc > 0:
-					print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1])," is a Stable Node")
-				elif det > 0 and trace > 0 and disc < 0:
-					print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1]), " is an Unstable Focus")
-				elif det > 0 and trace < 0 and disc < 0:
-					print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1])," is a Stable Focus")    
+            def partial_derivative(func,var = 0,point = []):
+                args = point[:]
+                def wraps(x):
+                    args[var] = x
+                    return funct(*args)
+                return derivative(wraps,point[var],dx = 1e-14)
+            for n in range(len(self.steadyStateArray)):
+                Jacobfx = partial_derivative(f,0,[self.steadyStateArray[n,0],self.steadyStateArray[n,1]])
+                Jacobgx = partial_derivative(g,0,[self.steadyStateArray[n,0],self.steadyStateArray[n,1]])
+                Jacobfy = partial_derivative(f,1,[self.steadyStateArray[n,0],self.steadyStateArray[n,1]])
+                Jacobgy = partial_derivative(g,1,[self.steadyStateArray[n,0],self.steadyStateArray[n,1]])
+                trace = Jacobfx + Jacobgy
+                det = ( Jacobfx * Jacobgy ) - ( Jacobfy * Jacobgx )
+                disc = trace**2 - 4*det
+                if det < 0:
+                    print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1]),"is a Saddle Node")
+                elif det > 0 and trace == 0:
+                    print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1])," is a Centre")
+                elif det > 0 and trace > 0 and disc > 0:
+                    print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1])," is an Unstable Node")
+                elif det > 0 and trace < 0 and disc > 0:
+                    print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1])," is a Stable Node")
+                elif det > 0 and trace > 0 and disc < 0:
+                    print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1]), " is an Unstable Focus")
+                elif det > 0 and trace < 0 and disc < 0:
+                    print("The Steady State at ",(self.steadyStateArray[n,0],self.steadyStateArray[n,1])," is a Stable Focus")    
 
     def setNullClines(self):
         return 0
+    def getSeparatricies(self,learning_rate=0.01,method = 'Analytic'):
+        self.countList = []
+        if method == 'Analytic':
+            for steadyState in self.steadyStateArray:
+                test = self.getJacobian(steadyState[0],steadyState[1])
+                det = ( test[0,0]* test[1,1] ) - ( test[0,1] * test[1,0] )
+                if det < 0:
+                    eigenvalues,eigenvectors = np.linalg.eig(test)
+                    x1m = steadyState[0] - eigenvectors[0][0] * learning_rate
+                    x1p = steadyState[0] + eigenvectors[0][0] * learning_rate
+                    y1m = steadyState[1] - eigenvectors[0][1] * learning_rate
+                    y1p = steadyState[1] + eigenvectors[0][1] * learning_rate
+                
+                    x2m = steadyState[0] - eigenvectors[1][0] * learning_rate
+                    x2p = steadyState[0] + eigenvectors[1][0] * learning_rate
+                    y2m = steadyState[1] - eigenvectors[1][1] * learning_rate
+                    y2p = steadyState[1] + eigenvectors[1][1] * learning_rate
+                    
+                  
+                    def dydt(t,y):
+                        y1,y2 = y
+                        dy1dt = self.f(y1,y2)
+                        dy2dt = self.g(y1,y2)
+                        return [dy1dt,dy2dt]
+                    sol1m = self.solve_ivp(dydt,[0,np.sign(eigenvalues[0])*100],[x1m,y1m])
+                    sol1p = self.solve_ivp(dydt,[0,np.sign(eigenvalues[0])*100],[x1p,y1p])
+                    sol2m = self.solve_ivp(dydt,[0,np.sign(eigenvalues[1])*100],[x2m,y2m])
+                    sol2p = self.solve_ivp(dydt,[0,np.sign(eigenvalues[1])*100],[x2p,y2p])
+                    self.countList.append(sol1m)
+                    self.countList.append(sol1p)
+                    self.countList.append(sol2m)
+                    self.countList.append(sol2p)
+                    
+            self.Seperatricies = True
+        if method == 'Numeric':
+            for steadyState in self.steadyStateArray:
+                return 0 
+                
+                
+        
     
     def plotoptions(self):
-		print("Plot Colours:")
-		print("Red = "r"")
-		print("Blue = "b"")
-		print("Green = "g"")
-		print("Cyan = "c"")
-		print("Magenta = "m"")
-		print("Yellow = "y"")
-		print("Black = "k"")
-		print("White = "w"")
-	
-	def setDrawTime(self):
+        print("Plot Colours:")
+        print("Red = r")
+        print("Blue = b")
+        print("Green = g")
+        print("Cyan = c")
+        print("Magenta = m")
+        print("Yellow = y")
+        print("Black = k")
+        print("White = w")
+
+    def setDrawTime(self):
         return 0
     
     def draw(self):
@@ -281,13 +322,15 @@ class twoDSystem:
                 ax1.plot(sol.y[0],sol.y[1],self.flowColour)
                 
         if self.steadyStates == True:
-            for n in range(len(self.steadyStateX)):
-                ax1.plot(self.steadyStateX[n],self.steadyStateY[n],'o',color = self.steadyStateColour)
-		
-		if self.numericalSteadyStates == True:
-			for n in range(len(self.steadyStateArray)):
-				ax1.plot(self.steadyStateArray[n,0],self.steadyStateArray[n,1],'o',color = self.steadyStateColour)
-				
-		
+            for steadyState in self.steadyStateArray:
+                ax1.plot(steadyState[0],steadyState[1],'o',color = self.steadyStateColour)
+        
+        if self.numericalSteadyStates == True:
+            for n in range(len(self.steadyStateArray)):
+                ax1.plot(self.steadyStateArray[n,0],self.steadyStateArray[n,1],'o',color = self.steadyStateColour)
+        if self.Seperatricies == True:
+            for sep in self.countList:
+                ax1.plot(sep.y[0],sep.y[1])
+
         return fig.show()
 
